4ced60b8fecbd84be0cda96815a68fdbe00cea42
DivyaRukmini <divyamoh@in.ibm.com>
2025-02-12 20:51:44 +0530

    Add files via upload

---
+++ TestNG/Activities/Activity4A.java
@@ -0,0 +1,15 @@
+package TestNG;
+
+import org.testng.annotations.Test;
+
+public class Activity4A {
+    @Test
+    public void firstTestCase() {
+        System.out.println("I'm in first test case from Activty4 A Class");
+    }
+
+    @Test
+    public void secondTestCase() {
+        System.out.println("I'm in second test case from Activty4 A Class");
+    }
+}
---
+++ TestNG/Activities/Activity4B.java
@@ -0,0 +1,9 @@
+package TestNG;
+import org.testng.annotations.Test;
+
+public class Activity4B {
+    @Test
+    public void TestCase() {
+        System.out.println("I'm in the test case from Activty4 B Class");
+    }
+}
